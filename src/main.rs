1d0cefbeb5055616ef8ef4a84ccfd9d4186a743b
nester-a <nester.a.l@yandex.ru>
2025-12-06 16:21:38 +0300

    add main structs and one function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Address {
+    dist: String,
+    str: String,
+    num: u8,
+}
+
+impl Address {
+    fn new(dist: String, str: String, num: u8) -> Self {
+        Address { dist, str, num }
+    }
+    fn is_same(&self, add: &Address) -> bool {
+        self.dist == add.dist && self.str== add.str && self.num == add.num
+    }
+}
+
+struct Building {
+    add: Address,
+    build_year: u8,
+}
+
+impl Building {
+    fn new(dist: String, str: String, num: u8, build_year: u8) -> Self {
+        Building { add: Address::new(dist, str, num), build_year }
+    }
+}
+
+fn is_unique(b: &Building, vec: &Vec<Building>) -> bool {
+    for cur in vec {
+        if cur.add.is_same(&b.add){
+            return false;
+        }
+    }
+    true
+}
